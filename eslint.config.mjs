3c468fcc2e8ace88ac4c15d69f4cba011a76dca6
onyxdude <leocerva29@gmail.com>
2024-10-08 15:50:20 -0600

    feat:agregar cnfig eslint prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
   {ignores: ["dist/"]},
@@ -9,5 +9,5 @@ export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tseslint.configs.recommended,eslintPluginPrettierRecommended,
 ];
